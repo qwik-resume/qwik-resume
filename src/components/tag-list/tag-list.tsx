26d0bbd381138a1964faf2695c59ba19af1ecd89
Chris <full-stack.dev@strolia-davis.us>
2023-07-20 09:52:18 -0400

    #18 | csd | Pass test for adding options to the tag list
    - Demonstrating TFD
      - Write a single test ✓
      - Make sure it fails ✓
      - Make it pass with the simplest code possible ✓
      - Refactor code for clarity/readability
      - Make sure it still passes
    
    Will complete the next 2 items in the next commit

--- src/components/tag-list/tag-list.tsx
+++ src/components/tag-list/tag-list.tsx
@@ -1,10 +1,17 @@
 import { component$ } from '@builder.io/qwik';
 
+export interface TagListProps {
+  data?: string[];
+}
+
 /**
  * TagList component
  */
-export const TagList = component$(() => (
+export const TagList = component$<TagListProps>((props: TagListProps = {}) => (
   <div class='tag-list border border-black shadow w-full rounded h-10 px-1 py-1'>
+    {props?.data?.map((item: string) => (
+      <div class='tag-list-item'>{item}</div>
+    ))}
     <div contentEditable='true'></div>
   </div>
 ));
